a11bc2923daf8011e1c5003967a8d919b3fc69c2
EC2 Default User <ec2-user@ip-172-31-46-202.us-west-2.compute.internal>
2021-11-11 01:33:21 +0000

    adding logic for lambda to get all channel status and update dynamo

--- amplify/backend/function/viewerCount/src/index.js
+++ amplify/backend/function/viewerCount/src/index.js
@@ -9,23 +9,29 @@ Amplify Params - DO NOT EDIT */
 //const AWS = require('aws-sdk');
 /* eslint-enable */
 //const ivsClient = new AWS.IVS({region: "us-west2"});
-const { IvsClient, GetStreamCommand } = require("@aws-sdk/client-ivs"); // ES Modules import
+const { IvsClient, GetStreamCommand, ListChannelsCommand } = require("@aws-sdk/client-ivs"); // ES Modules import
 
 const axios = require('axios');
 const gql = require('graphql-tag');
 const graphql = require('graphql');
 const { print } = graphql;
 
+const GRAPHQL_ENDPOINT = "https://mmwdcbsihjf43kovagirdoa6au.appsync-api.us-west-2.amazonaws.com/graphql"
+const API_KEY = "da2-momiu3347fepze2j57dj4yw7xm"
+const CHANNEL_ARN = "arn:aws:ivs:us-west-2:865446119418:channel/F0xJeGNpoRd7"
+
+
+/*
 const GRAPHQL_ENDPOINT = process.env.GRAPHQL_ENDPOINT
 const API_KEY = process.env.API_KEY
 const CHANNEL_ARN = process.env.CHANNEL_ARN
-
+*/
 
 //getViewerCount(channelArn: String!): viewerCount
 
 const getViewerCounts = gql `
-  query getViewerCount($CHANNEL_ARN : String!){
-    getViewerCount(channelArn : $CHANNEL_ARN){
+  query getViewerCount($channelArn : String!){
+    getViewerCount(channelArn : $channelArn){
       channelArn
       viewerCount
     }
@@ -63,79 +69,117 @@ exports.handler = async (event) => {
     
     const client = new IvsClient(config);
     
-    const params = {
-        channelArn: CHANNEL_ARN
-    };
+    const listChannelsInput = {}
+    const listChannelCommand = new ListChannelsCommand(listChannelsInput)
+    const listChannelResponse = await client.send(listChannelCommand)
+    //var listChannelResponseObject = JSON.parse(listChannelResponse)
     
-    //const command = new GetStreamCommand(params);
+    /*for(var arn in listChannelResponse["channels"]){
+      console.log(arn[0])  
+    }*/
     
-    const command = new GetStreamCommand(params);
-    const response1 =  client.send(command);
+    //console.log(listChannelResponse["channels"])
     
-    const streamResponse = await client.send(command);
+    //var arnList = []
     
-    console.log(streamResponse["stream"]["viewerCount"])
+    console.log(listChannelResponse["channels"].length)
     
-    
-  try {
-    const channelExistsGraphQlData = await axios({
-      url: GRAPHQL_ENDPOINT,
-      method: 'post',
-      headers: {
-        'x-api-key': API_KEY
-      },
-      data: {
-        query: print(getViewerCounts),
-        variables: {CHANNEL_ARN}
-      }
-    });
-    
-    //console.log(graphqlData.data.data.getViewerCount)
-    
-    if(!channelExistsGraphQlData.data.data.getViewerCount){
-      console.log("create")
+    listChannelResponse["channels"].forEach(async function(channel){
+      var arn = channel["arn"]
       
-      const createRecord = await axios({
-      url: GRAPHQL_ENDPOINT,
-      method: 'post',
-      headers: {
-        'x-api-key': API_KEY
-      },
-      data: {
-        query: print(createViewerCount),
-        variables: {
-          input: {
-            channelArn: CHANNEL_ARN,
-            viewerCount: streamResponse["stream"]["viewerCount"]
-            }
-          }
+      const params = {
+        channelArn: arn
+      };
+    
+     
+        const command = new GetStreamCommand(params);
+        //console.log(streamResponse)
+        
+        var viewerCount = -1
+        
+        try{
+          
+          const streamResponse = await client.send(command);
+          viewerCount = streamResponse["stream"]["viewerCount"]
+          
+        }catch (e){
+          //console.log(e)
+          
         }
-      });
-      
-    } else {
-      console.log("update")
-      
-      const updateRecord = await axios({
-      url: GRAPHQL_ENDPOINT,
-      method: 'post',
-      headers: {
-        'x-api-key': API_KEY
-      },
-      data: {
-        query: print(updateViewerCount),
-        variables: {
-          input: {
-            channelArn: CHANNEL_ARN,
-            viewerCount: streamResponse["stream"]["viewerCount"]
+        
+        
+        console.log(arn + " " + viewerCount)
+        
+        try {
+          const channelExistsGraphQlData = await axios({
+            url: GRAPHQL_ENDPOINT,
+            method: 'post',
+            headers: {
+              'x-api-key': API_KEY
+            },
+            data: {
+              query: print(getViewerCounts),
+              variables: {
+                channelArn: arn
+              }
             }
+          });
+          
+          console.log(channelExistsGraphQlData.data)
+          
+          if(!channelExistsGraphQlData.data.data.getViewerCount){
+            console.log("create")
+            
+            const createRecord = await axios({
+            url: GRAPHQL_ENDPOINT,
+            method: 'post',
+            headers: {
+              'x-api-key': API_KEY
+            },
+            data: {
+              query: print(createViewerCount),
+              variables: {
+                input: {
+                  channelArn: arn,
+                  viewerCount: viewerCount
+                  }
+                }
+              }
+            });
+            
+          } else {
+            console.log("update")
+            
+            const updateRecord = await axios({
+            url: GRAPHQL_ENDPOINT,
+            method: 'post',
+            headers: {
+              'x-api-key': API_KEY
+            },
+            data: {
+              query: print(updateViewerCount),
+              variables: {
+                input: {
+                  channelArn: arn,
+                  viewerCount: viewerCount
+                  }
+                }
+              }
+            });
           }
+          
+        } catch (err) {
+          console.log('error posting to appsync: ', err);
         }
-      });
-    }
+            
+        
+        
+        
+      
+      
+    })
     
-  } catch (err) {
-    console.log('error posting to appsync: ', err);
-  } 
+    
   
   const response = {
         statusCode: 200,
@@ -149,4 +193,4 @@ exports.handler = async (event) => {
     return response;
 };
 
-//exports.handler()
+exports.handler()
