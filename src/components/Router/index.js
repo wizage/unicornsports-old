f571c35e95813aa8bd8178d00b658d6a5b91b1dd
wizage <wizagesmax@gmail.com>
2021-11-17 00:06:16 -0800

    Adding right home file

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Redirect } from 'react-router-dom';
 import { AuthState } from '@aws-amplify/ui-components';
-import Home from '../home';
+import Home from '../Home';
 import Channel from '../Channel';
 import ChannelAdmin from '../Admin';
 import Login from '../Login';
